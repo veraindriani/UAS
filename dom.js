85b123fae2043c610b17baf3639b4e23f5322e15
veraindriani <89260993+veraindriani@users.noreply.github.com>
2023-12-21 00:09:06 +0700

    forst commit

---
+++ dom.js
@@ -0,0 +1,16 @@
+const galeri = document.querySelector('.galeri');
+const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('thumb');
+
+galeri.addEventListener('click', function(e){
+
+    // cek apakah yang di-klik adalah thumb
+
+    if(e.target.className == 'thumb'){
+        jumbo.src = e.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500);
+    }
+});
